1cb72f456b7bc0ac5deb9277a640d98caabd6d60
Axel Schneider <axel99092@t-online.de>
2020-02-05 21:13:20 +0100

    Update TVPlayerInstall.sh
    neuer Installer, alte TV Listen bleiben erhalten.

--- TVPlayerInstall.sh
+++ TVPlayerInstall.sh
@@ -1,14 +1,49 @@
 #!/bin/sh
+filename=$HOME/.local/share/LiveStream-TVPlayer-master/tv_listen
+if [ -d "$filename" ]
+then
+    echo "$filename gefunden, kopiere nach /tmp"
+    cp -rf $filename /tmp
+else
+    echo "$filename nicht gefunden"
+fi
+sharedapps=$HOME/.local/share/applications/
+if [ -d "$sharedapps" ]
+ then
+    echo "$sharedapps gefunden"
+else
+    echo "$sharedapps nicht gefunden"
+    mkdir $sharedapps
+fi
+echo "lösche TVPlayer2"
+rm -rf ~/.local/share/LiveStream-TVPlayer-master
 cd ~/.local/share/
-echo "TVPlayer herunterladen ..."
+echo "TVPlayer2 herunterladen ..."
 wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer-Deutsch/archive/master.zip
-echo "TVPlayer entpacken"
+echo "TVPlayer2 extrahieren"
 unzip -o master.zip
-echo "zip Datei wird entfernt"
+sleep 1
+echo "zip Datei löschen"
 rm master.zip
 mv ~/.local/share/LiveStream-TVPlayer-Deutsch-master ~/.local/share/LiveStream-TVPlayer-master
-cp ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.desktop ~/.local/share/applications
-echo "Senderliste aktualisieren"
-python3 ~/.local/share/LiveStream-TVPlayer-master/query_mv.py ~/.local/share/LiveStream-TVPlayer-master
-echo "TVPlayer wird gestartet ..."
+rf=/tmp/tv_listen
+if [ -d "$rf" ]
+then
+    echo "tv_listen wiederherstellen"
+    cp $rf -rf $HOME/.local/share/LiveStream-TVPlayer-master
+else
+    echo "$rf nicht gefunden"
+fi
+desktopfile=$HOME/.local/share/applications/TVPlayer2.desktop
+if [ -e "$desktopfile" ]
+then
+    echo "$desktopfile already exists"
+else
+    echo "$desktopfile not found"
+    cp $HOME/.local/share/LiveStream-TVPlayer-master/TVPlayer2.desktop $HOME/.local/share/applications
+fi
+rm ~/Downloads/TVPlayerInstall.sh
+echo "Sender aktualisieren ... "
+python3 ~/.local/share/LiveStream-TVPlayer-master/query_mv.py ~/.local/share/LiveStream-TVPlayer-master/
+echo "TVPlayer2 starten ... "
 python3 ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.py
